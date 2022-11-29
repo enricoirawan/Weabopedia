dbf860854743586ce3c44542435fc981d286d2ae
Enrico Irawan <enricoirawan@MacBook-Air-Enrico.local>
2022-11-29 22:23:41 +0700

    init project

---
+++ Weabopedia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weabopedia
+//
+//  Created by Enrico Irawan on 29/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
